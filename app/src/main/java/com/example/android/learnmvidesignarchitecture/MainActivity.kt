97453f2c1ed5ece8182bfd048ee65a7fd0007f0d
Heba Elsaid <hebaelsaid912@gmail.com>
2021-12-30 16:52:31 +0200

    v1

--- app/src/main/java/com/example/android/learnmvidesignarchitecture/MainActivity.kt
+++ app/src/main/java/com/example/android/learnmvidesignarchitecture/MainActivity.kt
@@ -3,19 +3,41 @@ package com.example.android.learnmvidesignarchitecture
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.lifecycleScope
 import com.example.android.learnmvidesignarchitecture.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
+
+    private val viewModel: AddNumberViewModel by lazy {
+        ViewModelProvider(this).get(AddNumberViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        render()
         binding.addNumBtn.setOnClickListener {
-            // send
+            lifecycleScope.launch {
+                viewModel.intentChannel.send(MainIntent.addNumber)
+            }
 
         }
     }
-    private fun render(){
 
+    private fun render() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.state.collect{
+                when(it){
+                    is MainViewState.Idle -> binding.numerTv.text = "Idle"
+                    is MainViewState.numResult -> binding.numerTv.text = it.num.toString()
+                    is MainViewState.Error -> binding.numerTv.text = it.result
+                }
+            }
+        }
     }
 }
